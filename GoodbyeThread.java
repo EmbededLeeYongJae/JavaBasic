688211f098fa2fcd90ac3da98100702ec8e20ee7
EmbededLeeYongJae <168073551+EmbededLeeYongJae@users.noreply.github.com>
2024-05-09 17:53:10 +0900

    Add files via upload

---
+++ GoodbyeThread.java
@@ -0,0 +1,16 @@
+package javabasic;
+
+public class GoodbyeThread extends Thread {
+	
+	@Override
+	public void run() {
+		while(true) {
+		System.out.println("Good bye");
+		try {
+			Thread.sleep(500);
+		}catch(InterruptedException ie) {
+			ie.printStackTrace();
+		}
+		}
+	}
+}
---
+++ HelloThread.java
@@ -0,0 +1,24 @@
+package javabasic;
+
+public class HelloThread extends Thread {
+	
+	@Override
+	public void run() {
+		
+		while(true) {
+			System.out.println("Hello");
+			try {
+				Thread.sleep(500);
+			}catch(InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
+		
+		
+		
+	
+	}
+	
+	
+
+}
